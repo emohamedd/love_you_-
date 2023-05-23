fbe4905cc47138ed86df2f58275cd89ffbe4daa9
emohamedd <a.elgaouej@gmail.com>
2023-05-23 20:14:02 +0100

    true love

--- love.c
+++ love.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
-
-int main()
-{
-	int i  = 0;
+int main() {
+	int i = 0;
 	char str[10];
-	printf("what is Your name\n");
-		scanf("%s", str);
-	printf("How Much DO you love me (1-10):\n");
-		scanf("%d", &i);
-	if (i <= 0)
-{
-
-		printf("Don't Need your love\n");
+	
+	printf("\033[0;33mwhat is Your name 😇\n\033[0m"); 
+	scanf("%s", str);
+	
+	printf("\033[0;35mHow Much DO you love me (1-10): 😘💓\n\033[0m");
+	scanf("%d", &i);
+	
+	if (i <= 0) {
+		printf("\033[0;32mDon't Need your love 👀\n\033[0m"); 
 		return 0;
-}
-	else
-		printf("I LOve you Very\n");
-	while(i > 0)
-	{
-		printf("\t   Very\n");
+	} else {
+		printf("\033[0;32mI LOve you Very\n\033[0m"); 
+	}
+	
+	while (i > 0) {
+		printf("\033[0;31m\t   Very\n\033[0m");
 		i--;
 	}
-	printf("\tMuch %s\n\n", str);
-	return (0);
+	
+	printf("\033[0;35m\tMuch %s 🥰\n\n\033[0m", str);
+	return 0;
 }
